21a2ca8a08ced99aa80d90b8ddd6cbdc8f662286
hebo33 <bohe0829@gmail.com>
2026-09-12 21:06:29 -0400

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,157 @@
+from implementation import *
+
+# A string representing a simple 4 by 4 game board.
+SIMPLE_BOARD_STRING = 'P-B-\n-BRB\n--BB\n-C--'
+
+
+def simple_board_setup() -> GameBoard:
+    """Set up a simple game board"""
+    b = GameBoard(4, 4)
+    b.setup_from_grid(SIMPLE_BOARD_STRING)
+    return b
+
+
+def test_simple_place_character() -> None:
+    """Test GameBoard.place_character by placing a single Raccoon."""
+    b = GameBoard(3, 2)
+    r = Raccoon(b, 1, 1)
+    assert b.at(1, 1)[0] == r  # requires GameBoard.at be implemented to work
+
+
+def test_simple_at() -> None:
+    """Test GameBoard.at on docstring example"""
+    b = GameBoard(3, 2)
+    r = Raccoon(b, 1, 1)
+    assert b.at(1, 1)[0] == r
+    p = Player(b, 0, 1)
+    assert b.at(0, 1)[0] == p
+
+
+def test_simple_str() -> None:
+    """Test GameBoard.__str__ for the simple board in SIMPLE_BOARD_STRING."""
+    b = simple_board_setup()
+    assert str(b) == 'P-B-\n-BRB\n--BB\n-C--'
+
+
+def test_simple_check_game_ended() -> None:
+    """Test GameBoard.check_game_ended on the docstring example"""
+    b = GameBoard(3, 2)
+    Raccoon(b, 1, 0)
+    Player(b, 0, 0)
+    RecyclingBin(b, 1, 1)
+    assert b.check_game_ended() is None
+    assert not b.ended
+    RecyclingBin(b, 2, 0)
+    score = b.check_game_ended()
+    assert b.ended
+    assert score == 11
+
+
+def test_simple_adjacent_bin_score() -> None:
+    """Test GameBoard.adjacent_bin_score on the docstring example"""
+    b = GameBoard(3, 3)
+    RecyclingBin(b, 1, 1)
+    RecyclingBin(b, 0, 0)
+    RecyclingBin(b, 2, 2)
+    assert b.adjacent_bin_score() == 1
+    RecyclingBin(b, 2, 1)
+    assert b.adjacent_bin_score() == 3
+    RecyclingBin(b, 0, 1)
+    assert b.adjacent_bin_score() == 5
+
+
+def test_simple_player_move() -> None:
+    """Test Player.move on docstring example."""
+    b = GameBoard(4, 2)
+    p = Player(b, 0, 0)
+    assert not p.move(UP)
+    assert p.move(DOWN)
+    assert b.at(0, 1) == [p]
+    RecyclingBin(b, 1, 1)
+    assert p.move(RIGHT)
+    assert b.at(1, 1) == [p]
+
+
+def test_simple_recyclingbin_move() -> None:
+    """Test RecyclingBin.move on docstring example."""
+    b = GameBoard(4, 2)
+    rb = RecyclingBin(b, 0, 0)
+    assert not rb.move(UP)
+    assert rb.move(DOWN)
+    assert b.at(0, 1) == [rb]
+
+
+def test_simple_raccoon_check_trapped() -> None:
+    """Test Raccoon.check_trapped on docstring example."""
+    b = GameBoard(3, 3)
+    r = Raccoon(b, 2, 1)
+    Raccoon(b, 2, 2)
+    Player(b, 2, 0)
+    assert not r.check_trapped()
+    RecyclingBin(b, 1, 1)
+    assert r.check_trapped()
+
+
+def test_simple_raccoon_move() -> None:
+    """Test Raccoon.move on docstring example."""
+    b = GameBoard(4, 2)
+    r = Raccoon(b, 0, 0)
+    assert not r.move(UP)
+    assert r.move(DOWN)
+    assert b.at(0, 1) == [r]
+    g = GarbageCan(b, 1, 1, True)
+    assert r.move(RIGHT)
+    assert (r.x, r.y) == (0, 1)  # Raccoon didn't change its position
+    assert not g.locked  # Raccoon unlocked the garbage can!
+    assert r.move(RIGHT)
+    assert r.inside_can
+    assert len(b.at(1, 1)) == 2  # Raccoon and GarbageCan are both at (1, 1)!
+
+
+def test_simple_raccoon_take_turn() -> None:
+    """Test Raccoon.take_turn on docstring example."""
+    b = GameBoard(3, 4)
+    r1 = Raccoon(b, 0, 0)
+    r1.take_turn()
+    assert (r1.x, r1.y) in [(0, 1), (1, 0)]
+    r2 = Raccoon(b, 2, 1)
+    RecyclingBin(b, 2, 0)
+    RecyclingBin(b, 1, 1)
+    RecyclingBin(b, 2, 2)
+    r2.take_turn()  # Raccoon is trapped
+    assert (r2.x, r2.y) == (2, 1)
+
+
+def test_simple_smartraccoon_take_turn() -> None:
+    """Test SmartRaccoon.take_turn on docstring example."""
+    b = GameBoard(8, 2)
+    s = SmartRaccoon(b, 4, 0)
+    GarbageCan(b, 3, 1, False)
+    GarbageCan(b, 0, 0, False)
+    GarbageCan(b, 7, 0, False)
+    s.take_turn()
+    assert s.x == 5
+    s.take_turn()
+    assert s.x == 6
+
+
+def test_simple_give_turns() -> None:
+    """Test GameBoard.give_turns on docstring example."""
+    b = GameBoard(4, 3)
+    p = Player(b, 0, 0)
+    r = Raccoon(b, 1, 1)
+    for _ in range(RACCOON_TURN_FREQUENCY - 1):
+        b.give_turns()
+    assert b.turns == RACCOON_TURN_FREQUENCY - 1
+    assert (r.x, r.y) == (1, 1)  # Raccoon hasn't had a turn yet
+    assert (p.x, p.y) == (0, 0)  # Player hasn't had any inputs
+    p.record_event(RIGHT)
+    b.give_turns()
+    assert (r.x, r.y) != (1, 1)  # Raccoon has had a turn!
+    assert (p.x, p.y) == (1, 0)  # Player moved right!
+
+
+if __name__ == '__main__':
+    import pytest
+
+    pytest.main(['testing.py'])
